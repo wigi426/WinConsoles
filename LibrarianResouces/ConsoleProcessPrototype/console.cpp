f9a2e209ef3a7b19663b7255a1fe860c6a41ff09
wigi426 <47248479+wigi426@users.noreply.github.com>
2022-11-16 08:13:28 +0000

    finished refactoring of cnosole process prototype, every stream is now converted into a std::i/ostream and is cleaned up on object destruction

--- LibrarianResouces/ConsoleProcessPrototype/console.cpp
+++ LibrarianResouces/ConsoleProcessPrototype/console.cpp
@@ -1,56 +1,74 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <io.h>
 #include <concepts>
+#include <memory>
+#include <cassert>
+#include <type_traits>
+#include <io.h>
 #include <Windows.h>
 
-template<typename T>
-concept isStdIO_Stream(std::same_as<T, std::istream> || std::same_as<T, std::ostream>);
+template<typename T, typename U>
+concept StdIO_Stream = (std::same_as<T, std::istream> && std::same_as<U, std::ifstream>)
+|| (std::same_as<T, std::ostream> && std::same_as<U, std::ofstream>);
 
-template<isStdIO_Stream T>
-class WinHANDLE_stdStream_association
+
+template<typename T, typename U>
+    requires StdIO_Stream<T, U>
+class WinHANDLE_stdStreamAssociation
 {
 public:
-    WinHANDLE_stdStream_association(HANLDE WinHndl);
+    WinHANDLE_stdStreamAssociation(HANDLE WinHndl);
+    ~WinHANDLE_stdStreamAssociation();
+    T& get() { return *m_stream.get(); }
 private:
-    T stream;
+    WinHANDLE_stdStreamAssociation(const WinHANDLE_stdStreamAssociation&) = delete;
+    WinHANDLE_stdStreamAssociation(WinHANDLE_stdStreamAssociation&&) = delete;
+    int m_fileDescriptor{ -1 };
+    FILE* m_fileStream{ nullptr };
+    std::unique_ptr<U> m_fstream{ nullptr };
+    std::unique_ptr<T> m_stream{ nullptr };
 };
 
-
-struct fileHANDLE {
-    fileHANDLE(_In_ LPCSTR lpFileName,
-        _In_ DWORD dwDesiredAccess,
-        _In_ DWORD dwShareMode,
-        _In_opt_ LPSECURITY_ATTRIBUTES lpSecurityAttributes,
-        _In_ DWORD dwCreationDisposition,
-        _In_ DWORD dwFlagsAndAttributes,
-        _In_opt_ HANDLE hTemplateFile) {
-        h = CreateFileA(lpFileName,
-            dwDesiredAccess,
-            dwShareMode,
-            lpSecurityAttributes,
-            dwCreationDisposition,
-            dwFlagsAndAttributes,
-            hTemplateFile);
-        if (h == INVALID_HANDLE_VALUE)
-            throw std::runtime_error(std::string("CreateFileA failed on file: \"") + lpFileName + '\"');
-    }
-    ~fileHANDLE()
+template<typename T, typename U>
+    requires StdIO_Stream<T, U>
+WinHANDLE_stdStreamAssociation<T, U>::WinHANDLE_stdStreamAssociation(HANDLE WinHndl)
+{
+    if (WinHndl == INVALID_HANDLE_VALUE)
+        throw std::runtime_error("Invalid WHANDLE");
+    m_fileDescriptor = _open_osfhandle(reinterpret_cast<intptr_t>(WinHndl), 0);
+    if (m_fileDescriptor == -1)
+        throw std::runtime_error("could not create file descriptor from HANDLE");
+    if constexpr (std::is_same<T, std::istream>::value)
     {
-        try {
-            CloseHandle(h);
-        }
-        catch (std::exception& e)
-        {
-            std::cout << "caught exception in fileHANLDE when closing assosiated win32 HANDLE, GetLastError(): " << GetLastError() << '\n' << "exception::what(): " << e.what() << '\n';
-        }
+        m_fileStream = _fdopen(m_fileDescriptor, "r");
     }
-    HANDLE get() { return h; }
+    else
+    {
+        m_fileStream = _fdopen(m_fileDescriptor, "a");
+    }
+    if (m_fileStream == nullptr)
+        throw std::runtime_error("coud not associate stream with file descriptor");
+    m_fstream = std::make_unique<U>(m_fileStream);
+    if (!m_fstream.get()->good())
+        throw std::runtime_error("could not create valid std::i/ofstream");
+    m_stream = std::make_unique<T>(m_fstream.get()->rdbuf());
+    if (!m_stream.get()->good())
+        throw std::runtime_error("could not create valid std::i/ostream");
+}
 
-private:
-    HANDLE h{ nullptr };
-};
+template<typename T, typename U>
+    requires StdIO_Stream<T, U>
+WinHANDLE_stdStreamAssociation<T, U>::~WinHANDLE_stdStreamAssociation()
+{
+    assert(fclose(m_fileStream) != EOF);
+    if (m_fstream.get()->is_open())
+    {
+        std::cout << "m_fstream needed closing, del this print\n";
+        m_fstream.get()->close();
+    }
+}
+
 
 class Console {
 public:
@@ -60,18 +78,22 @@ private:
     inline static std::istream sm_parentInStream{ std::cin.rdbuf() };
     inline static std::ostream sm_parentOutStream{ std::cout.rdbuf() };
 
-    inline static fileHANDLE sm_coutFileHandle{ "CONOUT$", GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL };
-    inline static fileHANDLE sm_cinFileHandle{ "CONIN$", GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL };
-
-    inline static int coutFD{ _open_osfhandle(reinterpret_cast<intptr_t>(sm_coutFileHandle.get()), 0) };
-    inline static int cinFD{ _open_osfhandle(reinterpret_cast<intptr_t>(sm_cinFileHandle.get()), 0) };
-
-    inline static FILE* coutFile{ _fdopen(coutFD, "a") };
-    inline static FILE* cinFile{ _fdopen(cinFD, "r") };
-
-    inline static std::ofstream fcout{ coutFile };
-    inline static std::ifstream fcin{ cinFile };
-
+    inline static WinHANDLE_stdStreamAssociation<std::ostream, std::ofstream> cout{ CreateFileA(
+        "CONOUT$",
+        GENERIC_WRITE,
+        0,
+        NULL,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL) };
+    inline static WinHANDLE_stdStreamAssociation<std::istream, std::ifstream> cin{ CreateFileA(
+        "CONIN$",
+        GENERIC_READ,
+        0,
+        NULL,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL) };
 };
 
 int main() {
@@ -87,12 +109,9 @@ bool Console::Init()
         return false;
     if (!sm_parentOutStream.good())
         return false;
-    std::cin.rdbuf(fcin.rdbuf());
-    if (!std::cin.good())
-        return false;
-    std::cout.rdbuf(fcout.rdbuf());
-    if (!std::cout.good())
-        return false;
+
+    std::cout.rdbuf(cout.get().rdbuf());
+    std::cin.rdbuf(cin.get().rdbuf());
 
     return true;
 }
